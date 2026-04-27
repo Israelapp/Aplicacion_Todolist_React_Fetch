58a379cc42517ef52dfdfc799b51d030d7e8bfa2
Israelapp <israelcastillo14c@gmail.com>
2026-04-27 12:35:27 +0200

    finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,38 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [list, setList] = useState([])
+
+	async function hello() {
+		let characters = await fetch("https://playground.4geeks.com/todo/users/IsraelApp")
+		let data = await characters.json()
+		setList(data.todos)
+	}
+	console.log(list)
+
+
+	useEffect(() => {
+		hello()
+	}, [])
+
 	return (
 		<div className="text-center">
-            
+			<h1>Lista de Tareas</h1>
+			<ul class="list-group">
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+				{
+					list.map((e) => {
+						return (
+							<li class="list-group-item">{
+								e.label}</li>
+						)
+					})
+				}
+
+			</ul>
+
+		</div >
 	);
 };
 
